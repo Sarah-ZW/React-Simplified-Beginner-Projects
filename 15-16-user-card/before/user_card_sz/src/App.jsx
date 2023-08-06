993ff58d7f22f572bb5d37ee70cdacb731037d76
Sarah-ZW <earthclaninfo@gmail.com>
2023-08-06 13:47:31 -0400

    usercard

--- 15-16-user-card/before/user_card_sz/src/App.jsx
+++ 15-16-user-card/before/user_card_sz/src/App.jsx
@@ -1,12 +1,18 @@
 import "./user.css"
-import "./user.html"
+import { UserCard } from "./UserCard"
 import user from "./user.json"
-
+console.log(JSON.stringify(user))
 function App() {
+  console.log
+  let userInfoString = JSON.stringify(user)
+  let userInfoObj = JSON.parse(userInfoString)
   return (
-    <>
-      <h1>{JSON.stringify(user)}</h1>
-    </>
+    <UserCard
+      name={userInfoObj.name}
+      age={userInfoObj.age}
+      phoneNumber={userInfoObj.phoneNumber}
+      address={userInfoObj.address}
+    />
   )
 }
 
