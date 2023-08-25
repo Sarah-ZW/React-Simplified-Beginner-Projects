32865b6665f13a65b7f6f60883cdd370b1538c2e
Sarah-ZW <earthclaninfo@gmail.com>
2023-08-25 16:32:05 -0400

    init

--- 37-38-user-list/before/user_list/src/App.jsx
+++ 37-38-user-list/before/user_list/src/App.jsx
@@ -3,21 +3,35 @@ import { User } from "./User"
 
 function App() {
   const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState()
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
+    setLoading(true)
+    const controller = new AbortController()
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal
+    })
+      .then((res) => res.json())
       .then(setUsers)
-  })
+      .finally(() => setLoading(false))
 
+      return () => {
+        controller.abort()
+      }
+  }, [])
+
+  let jsx
+  if (loading == true) {
+    jsx = "Loading..."
+  } else {
+    jsx = users.map((user) => {
+      return <User key={user.id} name={user.name} />
+    })
+  }
   return (
     <>
       <h1>User List</h1>
-      <ul>
-        {users.map(user => {
-          return <User key={user.id} name={user.name} />
-        })}
-      </ul>
+      <ul>{jsx}</ul>
     </>
   )
 }
