a8c06aaab2c4eeeff2f529312aff2fe88ec70adb
Sarah-ZW <earthclaninfo@gmail.com>
2023-08-27 20:46:14 -0400

    start of solo todo

--- 41-42-simple-todo-list/before/project/src/App.jsx
+++ 41-42-simple-todo-list/before/project/src/App.jsx
@@ -1,33 +1,33 @@
+import './styles.css'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [newItem, setNewItem] = useState('')
+  const [todos, setTodo] = useState([])
+  
+  const addTodo = (currentTodos) => {
+      return setTodo(...currentTodos, {id: crypto.randomUUID, name: newItem, completed: false})
+  }
+//next thing is to map out the todos
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <ul id="list">
+      <li className="list-item">
+        <label className="list-item-label">
+          <input type="checkbox" data-list-item-checkbox />
+          <span data-list-item-text >Item 1</span>
+        </label>
+        <button data-button-delete>Delete</button>
+      </li>
+    </ul>
+
+    <div id="new-todo-form">
+      <label htmlFor="todo-input">New Todo</label>
+      <input type="text" id="todo-input" value={newItem} onChange={(e) => setNewItem(e.value.target) } />
+      <button onClick={addTodo}>Add Todo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
