687400fb08d46a245b8336929ed33def8a969135
Sarah-ZW <earthclaninfo@gmail.com>
2023-08-28 20:42:33 -0400

    update

--- 41-42-simple-todo-list/before/project/src/App.jsx
+++ 41-42-simple-todo-list/before/project/src/App.jsx
@@ -1,33 +1,50 @@
-import './styles.css'
-import { useState } from 'react'
+import "./styles.css"
+import { useState } from "react"
 
 function App() {
-  const [newItem, setNewItem] = useState('')
-  const [todos, setTodo] = useState([])
-  
-  const addTodo = (currentTodos) => {
-      return setTodo(...currentTodos, {id: crypto.randomUUID, name: newItem, completed: false})
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function addTodo() {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), name: newItem, completed: false },
+      ]
+    })
+    if (newItem === "") {
+      return
+    } else {
+      setNewItem("")
+    }
   }
-//next thing is to map out the todos
+  //next thing is to set the value of completed using e.target.checked from the checkbox
   return (
     <>
-    
-    <ul id="list">
-      <li className="list-item">
-        <label className="list-item-label">
-          <input type="checkbox" data-list-item-checkbox />
-          <span data-list-item-text >Item 1</span>
-        </label>
-        <button data-button-delete>Delete</button>
-      </li>
-    </ul>
+      <ul id="list">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id} className="list-item">
+              <label className="list-item-label">
+                <input type="checkbox" value={todo.completed} data-list-item-checkbox />
+                <span data-list-item-text>{todo.name}</span>
+              </label>
+              <button data-button-delete>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
 
-    <div id="new-todo-form">
-      <label htmlFor="todo-input">New Todo</label>
-      <input type="text" id="todo-input" value={newItem} onChange={(e) => setNewItem(e.value.target) } />
-      <button onClick={addTodo}>Add Todo</button>
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">New Todo</label>
+        <input
+          type="text"
+          id="todo-input"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
       </div>
-
     </>
   )
 }
