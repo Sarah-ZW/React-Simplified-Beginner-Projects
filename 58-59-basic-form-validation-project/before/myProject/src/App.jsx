99ed4eef08fc25efca61d60370a124f8eacc36e1
Sarah-ZW <earthclaninfo@gmail.com>
2023-09-11 21:35:13 -0400

    start of build

--- 58-59-basic-form-validation-project/before/myProject/src/App.jsx
+++ 58-59-basic-form-validation-project/before/myProject/src/App.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react"
 
 function App() {
+  const [errorText, setErrorText] = useState()
+  const [email, setEmail] = useState()
+  // const [password, setPassword] = useState()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const emailRegex = /@webdevsimplified.com$/
+    const emailEnding = emailRegex.test(email)
+
+    if (!emailEnding) {
+      setErrorText("Please end your email address in @webdevsimplified.com")
+    }
+  }
 
   return (
-   <p>hiya!!!!</p>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group error">
+          <label className="label" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="input"
+            type="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+          <div className="msg" value={errorText}></div>
+        </div>
+        <div className="form-group">
+          <label className="label" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="input"
+            value="Password123!"
+            type="password"
+            id="password"
+          />
+        </div>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
   )
 }
 
