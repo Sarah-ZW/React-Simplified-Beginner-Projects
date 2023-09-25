a7352e448db3bbfb2ab56cf227c907d5f80d8482
Sarah-ZW <earthclaninfo@gmail.com>
2023-09-24 21:34:43 -0400

    halfway through

--- 68-69-advanced-todo-list/before/src/App.jsx
+++ 68-69-advanced-todo-list/before/src/App.jsx
@@ -1,43 +1,72 @@
-import { useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import "./styles.css"
 import { TodoItem } from "./TodoItem"
 
+const ACTIONS = {
+  Add: "ADD",
+  Update: "UPDATE",
+  Toggle: "TOGGLE",
+  Delete: "DELETE",
+}
+
+function reducer(todos, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD:
+      return [
+        ...todos,
+        { name: payload.name, completed: false, id: crypto.randomUUID() },
+      ]
+    case ACTIONS.TOGGLE:
+      return todos.map((todo) => {
+        if (todo.id === payload.id) {
+          return { ...todo, completed: payload.completed }
+        }
+
+        return todo
+      })
+    default:
+      throw new error(`Cannot find action for type ${type}.`)
+  }
+  return state
+}
+
 function App() {
   const [newTodoName, setNewTodoName] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, dispatch] = useReducer(reducer, [], (initialValue) => {
+    const storedArray = localStorage.getItem("storedTodos")
+    if (storedArray == null) {
+      return initialValue
+    } else {
+      return JSON.parse(storedArray)
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem("storedTodos", JSON.stringify(todos))
+  }, [todos])
 
   function addNewTodo() {
     if (newTodoName === "") return
 
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { name: newTodoName, completed: false, id: crypto.randomUUID() },
-      ]
-    })
+    dispatch({ type: ACTIONS.ADD, payload: { name: newTodoName } })
+
     setNewTodoName("")
   }
 
   function toggleTodo(todoId, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === todoId) return { ...todo, completed }
-
-        return todo
-      })
-    })
+    dispatch({ type: ACTIONS.TOGGLE, payload: { id: todoId, completed } })
   }
 
   function deleteTodo(todoId) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== todoId)
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== todoId)
     })
   }
 
   return (
     <>
       <ul id="list">
-        {todos.map(todo => {
+        {todos.map((todo) => {
           return (
             <TodoItem
               key={todo.id}
@@ -55,7 +84,7 @@ function App() {
           type="text"
           id="todo-input"
           value={newTodoName}
-          onChange={e => setNewTodoName(e.target.value)}
+          onChange={(e) => setNewTodoName(e.target.value)}
         />
         <button onClick={addNewTodo}>Add Todo</button>
       </div>
